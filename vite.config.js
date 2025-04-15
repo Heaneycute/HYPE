afc5bfcfc703952f3e69c83b8f9eab78e9f14d1c
Heaneycute <Heaneypower@gmail.com>
2025-04-15 17:13:29 +0300

    hot fix

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,5 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   plugins: [react()],
   base: "/HYPE/",
-  define: {
-    // Делаем переменные окружения доступными в клиентском коде
-    "process.env": process.env,
-  },
 });
+
